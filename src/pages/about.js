77c902652d2fbb8daaa0275085eab0cb2314d89a
SomaTomita <ldh.eeexile@icloud.com>
2024-01-19 23:57:37 +0900

    made files

---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+import Hero from "src/components/hero";
+
+export default function About() {
+	return <Hero />
+}
---
+++ src/pages/blog/index.js
@@ -0,0 +1,5 @@
+import Hero from "src/components/hero";
+
+export default function Blog() {
+	return <Hero />
+}
