f5cecf3a8c5a4dd41dc36deb185b9d4fee678cc0
SomaTomita <ldh.eeexile@icloud.com>
2024-01-21 23:47:44 +0900

    add styles

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import styles from "../styles/logo.module.css"
 
-export default function Logo() {
+export default function Logo({ boxOn = false }) {
 	return (
-		<Link href="/">CUBE</Link>
+		<Link href="/">
+			<span className={boxOn ? styles.box : styles.basic}>CUBE</span>
+		</Link>
 	);
 }
